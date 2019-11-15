c7d637eee8b3715e430152232d8a13d79873766f
Andre Wibbeke <andre.wibbeke@gmail.com>
2019-11-15 07:24:17 +0100

    feat: set up routing for user authentication components

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import SignIn from './components/Sign-In'
 import SignUp from './components/Sign-Up'
 import './App.css';
 
 export default () => {
   return (
-    <>
-      <h1>Hello World!</h1>
-      <SignIn />
-      <SignUp />
-    </>
+    <BrowserRouter>
+      <h1>Some fancy header</h1>
+      <Switch>
+        <Route path="/sign-in">
+          <SignIn />
+        </Route>
+        <Route path="/sign-up">
+          <SignUp />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   )
 }
