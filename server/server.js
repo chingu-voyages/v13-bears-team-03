4f6d708d3edcd30f3c111ad472fcc5d79c8fa887
tarek <atarek@bloopark.de>
2019-11-18 20:49:19 +0100

    add login api and auth with jwt

--- server/server.js
+++ server/server.js
@@ -1,31 +1,61 @@
-import bodyParser from 'body-parser'
-import express from 'express'
-import path from 'path'
-const app = express()
+import bodyParser from 'body-parser';
+import express from 'express';
+import path from 'path';
+require('dotenv').config();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+const app = express();
 
-const router = express.Router()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-const staticFiles = express.static(path.join(__dirname, '../../client/build'))
-app.use(staticFiles)
+const router = express.Router();
+const jwt = require('jsonwebtoken');
 
-router.get('/cities', (req, res) => {
-  const cities = [
-    {name: 'New York City', population: 8175133},
-    {name: 'Los Angeles',   population: 3792621},
-    {name: 'Chicago',       population: 2695598}
-  ]
-  res.json(cities)
-})
+const staticFiles = express.static(path.join(__dirname, '../../client/build'));
+app.use(staticFiles);
 
-app.use(router)
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader.split(' ')[1];
+  if (token === null) return res.sendStatus(401);
+  
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
+
+router.post('/login', (req, res) => {
+  //Authentication
+  const username = req.body.username;
+  const user = { name : username };
+  console.log(process.env.ACCESS_TOKEN);
+  const access_token = jwt.sign(user, process.env.ACCESS_TOKEN);
+  res.json(access_token);
+});
+
+const demo_content = [
+  {
+    username: 'user1',
+    content: 'content1',
+  },
+  {
+    username: 'user2',
+    content: 'content2'
+  }
+];
+
+router.get('/content', authenticateToken, (req, res) => {
+  res.json(demo_content.filter(content => content.username === req.user.name));
+});
+
+app.use(router);
 
 // any routes not picked up by the server api will be handled by the react router
-app.use('/*', staticFiles)
+app.use('/*', staticFiles);
 
-app.set('port', (process.env.PORT || 3001))
+app.set('port', (process.env.PORT || 3001));
 app.listen(app.get('port'), () => {
-  console.log(`Listening on ${app.get('port')}`)
-})
+  console.log(`Listening on ${app.get('port')}`);
+});
