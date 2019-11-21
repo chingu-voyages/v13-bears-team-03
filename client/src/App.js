202558b05c36dd59915763081dff7d9a582644e3
Andre Wibbeke <andre.wibbeke@gmail.com>
2019-11-21 07:48:13 +0100

    chore: remove some unnecessary files

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Header from './components/Header'
 import { SignUp, SignIn } from './components/Authentication'
-import './App.css';
 
 export default () => {
   return (
