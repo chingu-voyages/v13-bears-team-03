dbecd2e5ce2bbd2299592e0e28765578e5d5bd2a
Kish-Siva-1 <rodes88@msn.com>
2019-12-05 21:19:42 +0100

    will add search function

--- client/src/components/UserLandingPage/VehicleCard.js
+++ client/src/components/UserLandingPage/VehicleCard.js
@@ -9,14 +9,14 @@ class VehicleCard extends Component {
 
       <div key={1} > 
         <Card>
-          <Card.Img variant="top" src={"https://s29755.pcdn.co/wp-content/uploads/2019/08/2019_Top_Five_Class_5-Mack.jpg"} />
+          <Card.Img variant="top" src={this.props.imageUrl} />
           <Card.Body>
-            <Card.Title>{"Mack Class 8 Truck"}</Card.Title>
-            <Card.Subtitle > ${200} per day <br/> 
+            <Card.Title>{this.props.vehicle_model}</Card.Title>
+            <Card.Subtitle > ${this.props.vehicle_price} per day <br/> 
             Berlin
             </Card.Subtitle>
             <Card.Text>
-              {"This is a very popular new truck available for pickup from Spandau."}
+              {this.props.description}
             </Card.Text> 
           </Card.Body>
         </Card>
--- client/src/components/UserLandingPage/VehicleList.js
+++ client/src/components/UserLandingPage/VehicleList.js
@@ -1,54 +1,85 @@
 import React from 'react'
 import { Component } from 'react';
 import { CardColumns } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 import VehicleCard from './VehicleCard'
 import PageList from './PageList'
-import SearchBar from './SearchBar'
-import trucks1 from './FetchVariable'
+// import SearchBar from './SearchBar'
+// import trucks1 from './FetchVariable'
 import trucks2 from './TrucksList.json'
 
 class VehicleList extends Component {
 
+  state = {
+    trucks2: trucks2,
+    search_result: []
+  }
+
+  triggerList = () => {
+    if (this.state.search_result.length > 0) {
+      return this.state.search_result
+    }
+    else {
+      return this.state.trucks2
+    }
+  }
+
+  filterList = (event) => {
+
+    let items = trucks2.filter((item) => {
+      return item.vehicle_model.toLowerCase().search(event.target.value.toLowerCase()) !== -1;
+    });
+
+    this.setState({ search_result: items })
+
+  }
+
+  handleOnChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
   render() {
 
-    const trucks =
-    [
-        "Semi-Trailer",
-        "Straight Truck",
-        "Tesla Truck",
-        "Lowboy Truck"
-    ];
+    const trucks = this.triggerList().map((truck) => <VehicleCard key={truck._id} {...truck} />)
 
     return (
       <div>
-          <br />  
-          <div className="row justify-content-center">
-            <h2>Vehicles</h2>
-          </div>
-          <br />
-          <div className="col-md-12" align="left"> 
-                  <SearchBar content={trucks} />
-                </div>
-          <br />
-          <CardColumns>
-            {console.log(trucks1)}
-            <div className="container">
-              <div className="row" >
-                <div className="col-md-12" align="left"> 
-                  {<VehicleCard />} 
-                </div>
+        <br />
+        <div className="row justify-content-center">
+          <h2>Vehicles</h2>
+        </div>
+        <br />
+        <div className="col-md-12" align="left">
+
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Search</Form.Label>
+              <Form.Control type="search" name="search" onChange={this.filterList} placeholder="Enter vehicle name" />
+            </Form.Group>
+          </Form>
+
+        </div>
+        <br />
+        <CardColumns>
+          <div className="container">
+            <div className="row" >
+              <div className="col-md-12" align="left">
+                {trucks}
               </div>
             </div>
-          </CardColumns>
-
-          <div className="row justify-content-center">
-            <PageList />
           </div>
-        
+        </CardColumns>
+
+        <div className="row justify-content-center">
+          <PageList />
+        </div>
+
       </div>
     );
   }
-  
+
 }
 
 export default VehicleList;
